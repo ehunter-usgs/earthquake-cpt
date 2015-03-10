35bacf2037e66e9e48b0c36157eb1b1d63ffa933
Eric Martinez <emartinez@usgs.gov>
2015-03-10 09:51:24 -0600

    Added static content pages.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,6 +1,61 @@
 <?php
 if (!isset($TEMPLATE)) {
-  $TITLE = 'Page Title';
-  include 'template.inc.php';
+  $TITLE = "Cone Penetration Testing (CPT)";
+  $HEAD = '<link rel="stylesheet" href="../style.css"/>';
+
+  include_once 'template.inc.php';
 }
 ?>
+
+<figure class="right two-of-five">
+  <img src="images/truck_new.jpg" alt="image showing CPT rig and subsurface"/>
+  <figcaption>
+    Photo by Scott Haefner, USGS. Figure by Tom Holzer, USGS.
+  </figcaption>
+</figure>
+
+<p>
+  Cone Penetration Testing (CPT) is used to identify subsurface conditions in
+  the upper 100 ft of the subsurface. The USGS CPT uses a 23-ton truck to push
+  a &ldquo;<a href="cone.html" rel="cone">cone</a>&rdquo; into the ground.
+  The weight of the truck is partially supported by both the tip of the cone
+  and the sleeve of the cone. The &ldquo;tip resistance&rdquo; is determined by
+  the force required to push the tip of the cone and the &ldquo;sleeve
+  friction&rdquo; is determined by the force required to push the sleeve
+  through the soil. The &ldquo;friction ratio&rdquo; is the ratio between
+  sleeve friction and tip resistance, measured as a percentage. Soil type and
+  thereby resistance to liquefaction can be inferred from these measurements.
+</p>
+<p>
+  <a href="./data/">Data from the USGS cone</a>, which includes a
+  <a href="shaking.html" rel="shaking">seismometer</a>, can also be used to
+  predict how local shallow soil conditions can modify shaking. The capacity
+  of local soil conditions to modify shaking is inversely proportional to the
+  shear-wave velocity near the surface, which can be computed with data
+  recorded with the seismometer. Seismic energy is created manually with a
+  sledgehammer or automatically by a compressed air driven hammer. We measure
+  the time it takes for the seismic energy to travel from the land surface,
+  through the ground, to the seismometer mounted in the cone. The distance to
+  the seismometer divided by the travel time of the shear-wave is approximately
+  the average shear-wave velocity.
+</p>
+<p>
+  Output from the cone as it penetrates the soil is digitally recorded by a
+  computer and is collectively known as a sounding.
+</p>
+
+<h2>See also</h2>
+<ul>
+  <li>
+    USGS Factsheet:
+    <a href="http://geopubs.wr.usgs.gov/fact-sheet/fs028-03/">Subsurface
+        Exploration with the Cone Penetration Testing Truck</a>
+  </li>
+</ul>
+
+<h2 class="researcher">Scientific Staff</h2>
+<ul>
+  <li>Mike Bennett</li>
+  <li><a href="https://profile.usgs.gov/tholzer/">Tom Holzer</a></li>
+  <li>Tom Noce</li>
+</ul>
---
+++ src/htdocs/pubs.php
@@ -0,0 +1,208 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = "CPT Publications";
+  include_once 'template.inc.php';
+}
+?>
+
+<p>
+  If you use CPT data from this web site in a publication or thesis, please
+  send complete citation to <a href="mailto:tholzer@usgs.gov">Tom Holzer</a>
+  for posting on this web site.
+</p>
+
+<h2>Alameda County, California</h2>
+<ul class="referencelist">
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2010, Predicted liquefaction
+    in the Greater Oakland area and Northern Santa Clara Valley during a repeat
+    of the 1868 Hayward Fault (M6.7&ndash;7.0) earthquake, in Knudsen, K.,
+    Baldwin, J., Brocher, T., Burgmann, R., Craig, M., Cushing, D., Hellweg, P.,
+    Wiegers, M., and Wong, I., 3rd Conference on earthquake hazards in the
+    eastern San Francisco Bay Area: Hayward, CA, California Geological Survey
+    Special Report 219, v. Special Report 219, p. 147-163.
+  </li>
+  <li>
+    Bennett, M.J., Sneed, Michelle, Noce, T.E., and Tinsley, J.C. III, 2009,
+    Cone penetration test and soil boring at the Bayside Groundwater Project
+    in San Lorenzo, Alameda County, California: U.S. Geological Survey Open-File
+    Report 2009-1050, 25 p. and 3 spreadsheets,
+    <a href="http://pubs.usgs.gov/of/2009/1050/">http://pubs.usgs.gov/of/2009/1050/</a>.
+  </li>
+  <li>
+    Holzer, T.L., Bennett, M.J., Noce, T.E., Padovani, A.C., and Tinsley, J.C.,
+    III, 2006, Liquefaction hazard mapping with LPI in the greater Oakland,
+    California, area: Earthquake Spectra, v. 22, no. 3, 693-708.
+  </li>
+  <li>
+    Holzer, T.L., Blair, J.L., Noce, T.E., and Bennett, M.J., 2006, LiqueMap: A
+    real-time postearthquake map of liquefaction probability in National
+    Conference on Earthquake Engineering, 8th, San Francisco,
+    Proceedings, Paper 89, 10 p.</li>
+  <li>
+    Holzer, T.L., Blair, J.L., Noce, T.E., and Bennett, M.J., 2006, Predicted
+    liquefaction of East Bay fills during a repeat of a 1906 San
+    Francisco earthquake: Earthquake Spectra, v. 22, no. S2, p.
+    S261-S277.
+  </li>
+  <li>
+    Holzer, T.L., Bennett, M.J., Noce, T.E., and Tinsley, J.C., III, 2005,
+    Shear-wave velocity of surficial geologic sediments in Northern California:
+    Statistical distributions and depth dependence: Earthquake Spectra, v. 21,
+    no. 1, p. 161-177.
+  </li>
+  <li>
+    Holzer, T.L., Padovani, A.C., Bennett, M.J., Noce, T.E., and Tinsley, J.C.,
+    III, 2005, Mapping NEHRP VS30 site classes: Earthquake Spectra, v. 21, no.
+    2., p. 353-370.
+  </li>
+  <li>
+    ---, 2003, The application of the liquefaction potential index to
+    liquefaction hazard mapping, in Proceedings, Eighth U.S.-Japan workshop on
+    earthquake resistant design of lifeline facilities and countermeasures
+    against liquefaction, Tokyo, Multidisciplinary Center for Earthquake
+    Engineering Technical Report MCEER-03-0003, p. 161-171.</li>
+  <li>Holzer, T.L., Bennett, M.J., Noce, T.E., Padovani, A.C., and Tinsley,
+    J.C., III, 2002, Liquefaction hazard and shaking amplification maps of
+    Alameda, Berkeley, Emeryville, Oakland, and Piedmont: A digital database:
+    U.S. Geological Survey Open-file Report 02-296,
+    <a href="http://geopubs.wr.usgs.gov/open-file/of02-296/">http://geopubs.wr.usgs.gov/open-file/of02-296/</a>.
+  </li>
+  <li>
+    Holzer, T.L., Toprak, S., and Bennett, M.J., 2002, Liquefaction potential
+    index and seismic hazard mapping in the San Francisco Bay area, California,
+    in Proceedings, 7th National Conference on Earthquake Engineering, Boston,
+    v. II, p. 1699-1706.
+  </li>
+  <li>
+    Piratheepan, P., 2002, Estimating shear-wave velocity from SPT and CPT
+    data: M.S. Thesis, Department of Civil Engineering, Clemson University,
+    Clemson, SC,184 p.
+  </li>
+</ul>
+
+<h2>Evansville</h2>
+<ul class="referencelist">
+  <li>
+    Haase, J.S., Choi, Y.S., and Nowack, R.L., 2011, Liquefaction hazard near
+    the Ohio river from midwestern scenario earthquakes: Environmental and
+    Engineering Geoscience, v. 17, no. 2, p. 165-181.
+  </li>
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2011, Liquefaction probability
+    curves for surficial geologic units: Environmental and Engineering
+    Geoscience, v. 17, no. 1, p. 1-21.
+  </li>
+</ul>
+
+<h2>San Luis Obispo County, California</h2>
+<ul class="referencelist">
+  <li>
+    Holzer, T.L., Noce, T.E., Bennett, M.J., and Tinsley, J.C., III, Rosenberg,
+    L.I., 2005, Liquefaction at Oceano, California, during the 2003 San Simeon
+    earthquake: Bulletin Seismological Society of America, v. 95, no. 6, p.
+    2396-2411.
+  </li>
+  <li>
+    Holzer, T.L., Noce, T.E., Bennett, M.J., Di Alessandro, C., Boatwright, J.,
+    Tinsley, J.C., III, Sell, R.W., and Rosenberg, L.I., 2004,
+    Liquefaction-induced lateral spreading at Oceano, California, during the
+    2003 San Simeon earthquake: U.S. Geological Survey Open-file Report
+    2004-1269,
+    <a href="http://pubs.usgs.gov/of/2004/1269/">http://pubs.usgs.gov/of/2004/1269/</a>.
+  </li>
+</ul>
+
+<h2>Santa Clara County</h2>
+<ul class="referencelist">
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2010, Predicted liquefaction
+    in the Greater Oakland area and Northern Santa Clara Valley during a
+    repeat of the 1868 Hayward Fault (M6.7&ndash;7.0) earthquake, in Knudsen,
+    K., Baldwin, J., Brocher, T., Burgmann, R., Craig, M., Cushing, D.,
+    Hellweg, P., Wiegers, M., and Wong, I., 3rd Conference on earthquake
+    hazards in the eastern San Francisco Bay Area: Hayward, CA, California
+    Geological Survey Special Report 219, v. Special Report 219, p. 147-163.
+  </li>
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2009, Scenario liquefaction
+    hazard maps of Santa Clara Valley, Northern California: Seismological
+    Society of America Bulletin, v. 99, no. 1, p. 367-381.
+  </li>
+  <li>
+    Holzer, Thomas L., Noce, Thomas E., and Bennett, Michael J., 2008,
+    Liquefaction hazard maps for three earthquake scenarios for the
+    communities of San Jose, Campbell, Cupertino, Los Altos, Los Gatos,
+    Milpitas, Mountain View, Palo Alto, Santa Clara, Saratoga, and Sunnyvale,
+    northern Santa Clara County, California:  U.S. Geological Survey Open-file
+    Report 2008-1270, 33 p., 3 plates, and database,
+    <a href="http://pubs.usgs.gov/of/2008/1270/">http://pubs.usgs.gov/of/2008/1270/</a>.
+  </li>
+</ul>
+
+<h2>Solano County, California</h2>
+<ul class="referencelist">
+  <li>
+    Bennett, M.J., Noce, T.E., and Lienkaemper, J. J., 2011, Cone penetration
+    tests and soil borings at the Mason Road site in Green Valley, Solano
+    County, California; U.S. Geological Survey
+    <a href="http://pubs.usgs.gov/of/2011/1281/">Open-File Report 2011–1281</a>,
+    52 p. and 3 data tables.
+  </li>
+</ul>
+
+
+<h2>General</h2>
+<ul class="referencelist">
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2011, Liquefaction probability
+    curves for surficial geologic units: Environmental and Engineering
+    Geoscience, v. 17, no. 1, p. 1-21.
+  </li>
+  <li>
+    Holzer, T.L., Noce, T.E., and Bennett, M.J., 2010, Maps and documentation
+    of seismic CPT soundings in the eastern, central, and western United
+    States: U.S. Geological Survey Open-file Report 2010-1136, 63 p., 3 tables,
+    <a href="http://pubs.usgs.gov/of/2010/1136/">http://pubs.usgs.gov/of/2010/1136/</a>.
+  </li>
+  <li>
+    Holzer, T.L., 2008, Probabilistic liquefaction hazard mapping, in Zeng,
+    D., Manzari, M.T., and Hiltunen, D., eds., Geotechnical Earthquake
+    Engineering and Soil Dynamics Conference, 4th, Sacramento, Proceedings,
+    ASCE Geotechnical Special Publication No. 181, p. 1-32.
+  </li>
+  <li>
+    Holzer, T.L., and Bennett, M.J., 2007, Geologic and hydrogeologic
+    controls of boundaries of lateral spreads: Lessons from USGS case
+    histories, in, Schaefer, V.R., Schuster, R.L., and Turner, A.K., eds.,
+    First North American Landslide Conference, Proceedings, Association of
+    Engineering Geologists Special Publication 23, p. 502-522.
+  </li>
+  <li>
+    Holzer, T.L., and Youd, T.L., 2007, Liquefaction, ground oscillation,
+    and soil deformation at the Wildlife Array, California: Seismological
+    Society of America Bulletin, v. 97, no. 3, p. 961-976.
+  </li>
+  <li>
+    Noce, T.E., and Holzer, T.L., 2003, CPT-hole closure: Ground water
+    monitoring &amp; remediation, v. 23, no. 1, p. 93-96.
+  </li>
+  <li>
+    Toprak, S., and Holzer, T.L., 2003, Liquefaction potential index:
+    Field assessment: Journal of Geotechnical and Geoenvironmental
+    Engineering, v. 129, no. 4, p. 315-322.
+  </li>
+  <li>
+    ---, 2003, Subsurface exploration with the cone penetration testing
+    truck: U.S. Geological Survey Fact Sheet 028-03, 2 p.,
+    <a href="http://geopubs.wr.usgs.gov/fact-sheet/fs028-03/">http://geopubs.wr.usgs.gov/fact-sheet/fs028-03/</a>.
+  </li>
+  <li>
+    Toprak, S., Holzer, T.L., Bennett, M.J., and Tinsley, J.C., III, 1999,
+    CPT- and SPT-based probabilistic assessment of liquefaction potential,
+    <em>in</em> Proceedings, Seventh U.S.-Japan workshop on earthquake
+    resistant design of lifeline facilities and countermeasures against
+    liquefaction, Seattle, Multidisciplinary Center for Earthquake
+    Engineering Technical Report MCEER-99-0019, p. 69-86.
+  </li>
+</ul>
